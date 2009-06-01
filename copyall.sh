3fca44de40783fb9b4b8aeaa2adf0f1052a301bd
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-06-01 18:30:02 -0300

    deploys to 66 also

--- copyall.sh
+++ copyall.sh
@@ -1,4 +1,5 @@
 scp server/target/integracao-server.war integra@192.168.0.159:~/
 scp client/target/integracao-client.war integra@192.168.0.159:~/
+scp client/target/integracao-client.war integra@192.168.0.66:~/
 scp client/target/integracao-client.war integra@192.168.0.2:~/
 
