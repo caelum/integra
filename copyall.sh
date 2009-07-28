1961397ccf513e7c1ef995d3d7c7287fabf44439
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-07-28 14:39:38 -0300

    fixing dirs

--- copyall.sh
+++ copyall.sh
@@ -1,8 +1,8 @@
 ant war
 
 
-scp server/target/integracao-server.war integra@192.168.0.159:~/ 
-scp client/target/integracao-client.war integra@192.168.0.159:~/
+scp integracao-server/target/integracao-server.war integra@192.168.0.159:~/ 
+scp integracao-client/target/integracao-client.war integra@192.168.0.159:~/
 
 echo Restarting 159
 ssh integra@192.168.0.159 ./deploy.sh > /dev/null
@@ -30,9 +30,7 @@ ssh integra@192.168.0.123 ./deploy.sh > /dev/null
 scp integra@192.168.0.159:~/integracao-client.war integra@192.168.0.160:~/        
 scp integra@192.168.0.159:~/integracao-client.war integra@192.168.0.163:~/
 scp integra@192.168.0.159:~/integracao-client.war integra@192.168.0.161:~/        
-scp integra@192.168.0.159:~/integracao-client.war integra@192.168.0.164:~/
 scp integra@192.168.0.159:~/integracao-client.war integra@192.168.0.162:~/        
-scp integra@192.168.0.159:~/integracao-client.war integra@192.168.0.165:~/
 
 
 
@@ -51,10 +49,4 @@ ssh integra@192.168.0.162 ./deploy.sh > /dev/null
 
 echo Restarting 163
 ssh integra@192.168.0.163 ./deploy.sh
-
-echo Restarting 164
-ssh integra@192.168.0.164 ./deploy.sh > /dev/null
-
-echo Restarting 165
-ssh integra@192.168.0.165 ./deploy.sh > /dev/null
 
